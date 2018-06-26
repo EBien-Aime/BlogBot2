3f6b1370bfc3e8d915e89a423563720ca1d6cd66
emilyjennings <lanette.delbridge@yahoo.com>
2018-06-26 12:11:15 -0400

    edit user/comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
 	def create
 		@post = Post.find(params[:post_id])
 	 	@comment = @post.comments.create(params[:comment].permit(:name, :comment))
-		redirect_to post_path(@post)	
+		redirect_to post_path(@post)
 	end
 
 	def destroy
@@ -12,4 +12,34 @@ class CommentsController < ApplicationController
 		@comment.destroy
 		redirect_to post_path(@post)
 	end
+
+
+def show
+		@post = Post.find(params[:post_id])
+		@comments = Comment.find(params[:id])
+	end
+
+def edit
+	@post = Post.find(params[:post_id])
+	@comment = Comment.find(params[:id])
+	end
+
+def update
+	@post = Post.find(params[:post_id])
+	@comment = Comment.find(params[:id])
+
+	if @comment.update(comment_params)
+		flash[:notice]="Comment Updated Sucessfully"
+		redirect_to root_path
+	else
+		render 'edit'
+		end
+	end
+
+	def comment_params
+	  params.require(:comment).permit(:name, :comment)
+	end
+
+
+
 end
